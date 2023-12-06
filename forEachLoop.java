b91cd02f234b81d1bf8be0fc7d5f6b85ebacf8ba
elmntr <145943827+elmntr@users.noreply.github.com>
2023-12-06 08:45:33 +0800

    Add files via upload

---
+++ forEachLoop.java
@@ -0,0 +1,15 @@
+class forEachLoop{
+public static void main(String[] args){
+	
+	int[] numbers = {1,2,3,4,5,6,7,8,9,10,11,12};
+	int sum = 0;
+	
+	for(int x:numbers){
+		sum = sum + x;
+		
+	}
+	System.out.println(sum);
+	
+	
+}//end of main method	
+}//end of class
